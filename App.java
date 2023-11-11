71dd3a0f7080ba0dbc7755608dbc940ca6d8fe13
js12335 <97676697+js12335@users.noreply.github.com>
2023-11-11 13:18:22 +0530

    Add files via upload
    Basic Java Program

---
+++ App.java
@@ -0,0 +1,15 @@
+package TestDemo.com.jaya;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
